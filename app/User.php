4ea7236f2cc97e1e04468377d8a248202ad2d497
ahmed adel <abo3adel35@gmail.com>
2020-04-23 13:49:38 +0200

    add missing user methods

--- app/User.php
+++ app/User.php
@@ -6,12 +6,17 @@ use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Laravel\Lumen\Auth\Authorizable;
 use Laravel\Passport\HasApiTokens;
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
     use HasApiTokens,Authenticatable, Authorizable;
+
+    public const AdminRole = 2; // delete | edit | add 
+    public const SuperRole = 1; // edit | add
+    public const NormalUser = 0;
 
     /**
      * The attributes that are mass assignable.
@@ -30,4 +35,24 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     protected $hidden = [
         'password',
     ];
+
+    public function isAdmin(): bool
+    {
+        return (int)$this->role === self::AdminRole;
+    }
+
+    public function isSuper(): bool
+    {
+        return (int)$this->role === self::SuperRole;
+    }
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
